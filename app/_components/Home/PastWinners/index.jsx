7b9bbea1485c5f4fbe60a5c48e29ab2a7b746215
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2024-07-13 10:27:27 +0530

    past-winners update

--- app/_components/Home/PastWinners/index.jsx
+++
@@ -1,9 +0,0 @@
-// TODO: Create a Past Winners component.
-export default function PastWinners() {
-    return (
-      <main>
-        <h1 id="past-winners" className="text-6xl font-bold">Past Winners</h1>
-      </main>
-    );
-  }
-  
--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import CountdownTimer from "./_components/Home/CountdownTimer";
 import Eligibility from "./_components/Home/Eligibility";
 import Gallery from "./_components/Home/Gallery";
 import Hero from "./_components/Home/Hero";
-import PastWinners from "./_components/Home/PastWinners";
+import PastWinners from "./_components/Home/PastWinner";
 import Sponsor from "./_components/Home/Sponsor";
 import Timeline from "./_components/Home/Timeline";
 //
