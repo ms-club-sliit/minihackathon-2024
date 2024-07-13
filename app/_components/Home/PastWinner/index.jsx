ff3f9fc853d9be4e91ce76730b21c06bfe3a2373
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2024-07-13 23:18:31 +0530

    update responsiveness

--- app/_components/Home/PastWinner/index.jsx
+++ app/_components/Home/PastWinner/index.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import PastWinnersData from "../../data/PastWinners.json";
+
 const PastWinner = () => {
   return (
     <>
       {PastWinnersData.data.map((winner) => (
         <div
           key={winner.id}
-          className="grid grid-cols-1 md:grid-cols-2 mb-5 md:mb-10 px-7 py-7 border-2 border-gray-200 rounded-3xl "
+          className="grid grid-cols-1 md:grid-cols-2 mb-5  px-7 py-7 border-2 border-gray-200 rounded-3xl max-w-6xl mx-auto"
         >
           {winner.id % 2 === 0 ? (
             <>
-              <div className="flex self-center px-7 hidden md:block ">
-                <h1 className="italic text-xl md:text-xl text-justify mb-3 font-bold text-blue-500">{`Winners of ${winner.year}`}</h1>
-                <p className="italic text-xl md:text-xl text-justify">{`"${winner.description}"`}</p>
-                <h1 className="text-xl md:text-xl font-bold text-justify-right mt-5">{`-${winner.name}-`}</h1>
+              <div className="flex flex-col justify-center hidden md:block px-7">
+                <h1 className="italic text-xl mb-3 font-bold text-blue-500">{`Winners of ${winner.year}`}</h1>
+                <p className="italic text-xl text-justify">{`"${winner.description}"`}</p>
+                <h1 className="text-xl font-bold text-right mt-5">{`-${winner.name}-`}</h1>
               </div>
               <div className="flex justify-end">
                 <img
@@ -22,10 +23,10 @@ const PastWinner = () => {
                   className="min-w-0 max-w-lg md:max-w-lg md:rounded-xl px-7 py-7"
                 />
               </div>
-              <div className="flex self-center px- block md:hidden">
-			  <h1 className="italic text-xl md:text-xl text-justify mb-3 font-bold text-blue-500">{`Winners of ${winner.year}`}</h1>
-                <p className="italic text-xl md:text-xl text-justify">{`"${winner.description}"`}</p>
-                <h1 className="text-xl md:text-xl font-bold text-justify-right mt-5">{`-${winner.name}-`}</h1>
+              <div className="flex flex-col justify-center block md:hidden px-7">
+                <h1 className="italic text-xl mb-3 font-bold text-blue-500">{`Winners of ${winner.year}`}</h1>
+                <p className="italic text-xl text-justify">{`"${winner.description}"`}</p>
+                <h1 className="text-xl font-bold text-right mt-5">{`-${winner.name}-`}</h1>
               </div>
             </>
           ) : (
@@ -34,11 +35,13 @@ const PastWinner = () => {
                 <img
                   src={winner.imageUrl}
                   alt="Mini Hackathon Logo"
-                  className="min-w-0 max-w-lg md:max-w-lg md:rounded-xl px-14 py-14"
+                  className="min-w-0 max-w-lg md:max-w-lg md:rounded-xl px-7 py-7"
                 />
               </div>
-              <div className="flex self-center px-7">
-                <p className="italic text-xl md:text-2xl text-justify">{`"${winner.description}"`}</p>
+              <div className="flex flex-col justify-center px-7">
+                <h1 className="italic text-xl mb-3 font-bold text-blue-500">{`Winners of ${winner.year}`}</h1>
+                <p className="italic text-xl text-justify">{`"${winner.description}"`}</p>
+                <h1 className="text-xl font-bold text-right mt-5">{`-${winner.name}-`}</h1>
               </div>
             </>
           )}
