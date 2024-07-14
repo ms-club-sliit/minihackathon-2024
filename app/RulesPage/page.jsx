4f5ca0193a5a509b81f87f4d2d0ab53e520da59b
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-07-14 15:05:33 +0530

    styles added

--- app/RulesPage/page.jsx
+++ app/RulesPage/page.jsx
@@ -1,10 +1,12 @@
 import Container from '@/components/Container'
+// import Container2 from '@/components/Container2'
 import React from 'react'
 
 const RulesPage = () => {
   return (
     <div>
       <Container />
+      {/* <Container2/> */}
     </div>
   )
 }
---
+++ components/Container2.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Container2 = () => {
+    return (
+        <div className=''>
+            <h2 className='mt-10 mb-5 text-4xl font-bold'>ELIGIBILITY</h2>
+            <p className='sm:ml-10'>You are eligible to enter if you meet the following requirements below at the time of entry. For purposes of this Competition, "time of entry" is defined as the most recent date you have submitted an entry into a specific round.</p>
+            <ul className='list-disc sm:ml-20'>
+                <li>You are at least 16 years of age as of 21 July 2023 and are actively enrolled as a student following an undergraduate degree program at Srilanka Institute of Information Technology</li>
+                <li>You have not used paid employee's assistance in creating the mini hackathon entry deliverables</li>
+                <li>You are not actively enrolled in any administration role of this competition</li>
+                <li>You have not utilized any other competitors IPOs without their permission</li>
+                <li>You have not used any stolen/unauthorized resources in creating the deliverables for the competition</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Container2
