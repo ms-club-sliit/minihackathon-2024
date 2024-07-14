76281a27588ab97c5c3264a068f06820c2a52700
iamakmal <m.akmalalavi@gmail.com>
2024-07-14 21:52:42 +0530

    style: developed the base design

--- app/_components/Home/Timeline/index.jsx
+++ app/_components/Home/Timeline/index.jsx
@@ -1,64 +1,52 @@
 "use client"
+import {useEffect} from 'react'
 import TimelineData from '@/app/data/Timeline'
-import TimelineItem from '@/app/_components/Home/Timeline/TimelineItem'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Timeline() {
-  const TimelineNode = ({ top }) => (
-    <div 
-      className="absolute left-1/2 transform -translate-x-1/2 w-4 h-4 bg-blue-500 rounded-full border-4 border-white"
-      style={{ top: `${top}%` }}
-    ></div>
-  );
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
-    <main>
-      <h1 id="timeline" className="text-6xl font-bold">Timeline</h1>
-      <VerticalTimeline lineColor='#fff'>
-      {
-        TimelineData.map((timeline,index)=>(
- 
-          <VerticalTimelineElement
-          key={index}
-          visible={true}
-            className="vertical-timeline-element--work"
-            contentStyle={{ boxShadow:'0px 2px 9px #807E7E', borderRadius:'15px'}}
-            contentArrowStyle={{ borderLeft: '10px solid  #0080FF', border: '10px solid  #0080FF', borderRadius:"20px 20px", }}
-            //date={timeline.date}
-            iconStyle={{ background: '#0080FF', borderStyle: 'solid', borderWidth: '10px', borderColor: '#434040' }}
-            //icon={timelineIcon}
-          >
-            <h3 className="text-[10px] text-[#0080FF] font-semibold">{timeline.date}</h3>
-            <h4 className="text-[14px] text-[#161414] font-bold">{timeline.title}</h4>
-            <p className="text-[9px] text-[#334155] font-semibold">
-              {timeline.description}
-            </p>
-          </VerticalTimelineElement>
-        
-        ))
-      }
+    <main className="relative custom-gradient mb-20">
+      <h1 id="timeline" className="text-2xl font-bold text-center mt-20 text-[#1E293B] min-[744px]:text-[38px] min-[1440px]:text-5xl">Timeline</h1>
+      <img src="images/robot-top.png" className="h-[97px] md:ml-12 lg:mx-auto lg:pl-10 min-[744px]:h-[171px] min-[744px]:w-[241px] min-[1440px]:h-[258px] min-[1440px]:w-[360px]" />
+      <img src="images/design-left.png" className="absolute opacity-[10%] left-0 h-[220px] w-[350px] "/>
+      <img src="images/design-right.png" className="absolute opacity-[10%] right-0 h-[220px] w-[350px]"/>
+      <VerticalTimeline animate={true} className="custom">
+        {
+          TimelineData.map((timeline, index) => (
+            <div data-aos={timeline.position =="left" ? "fade-left": "fade-right" }>
+              <VerticalTimelineElement
+                position={timeline.position}
+                key={index}
+                visible={true}
+                className="vertical-timeline-element--work z-10 custom-sm-card"
+                contentStyle={{ boxShadow: '0px 2px 9px #807E7E', borderRadius: '15px', marginBottom: '18px', height:"fit"}}
+                contentArrowStyle={{ borderRight: '10px solid  #0080FF',}}
+                //className="bg-gray-500"
+                iconClassName="custom-icon bg-[#0080FF] border-solid border-[10px] lg:border-[15px] border-[#434040]"
+              >
+                <div className="">
+                <h3 className="text-[10px] text-[#0080FF] font-semibold xl:mb-2 min-[744px]:text-[13px] min-[1440px]:text-[20px]">{timeline.date}</h3>
+                <h4 className="text-[14px] text-[#161414] font-bold xl:leading-8 xl:mb-4 min-[744px]:text-[20px] min-[1440px]:text-[36px]">{timeline.title}</h4>
+                <h5 className="text-[10px] text-[#334155] font-semibold text-justify min-[744px]:text-[13px] min-[1440px]:text-[20px]">
+                  {timeline.description}
+                </h5>
+                </div>
+              </VerticalTimelineElement>
+            </div>
+          ))
+        }
       </VerticalTimeline>
-{/* <div className="container mx-auto p-4 lg:p-8">
-      <div className="relative">
-        // Gradient vertical line 
-        <div className="absolute left-4 lg:left-1/2 top-0 bottom-0 w-1 lg:-translate-x-1/2">
-          <div className="h-full w-full bg-gradient-to-b from-blue-500 via-blue-300 to-blue-500 rounded-full shadow-lg shadow-blue-300/50"></div>
-        </div>
-        
-        // Timeline items 
-        {TimelineData.map((event, index) => (
-          <div key={index} className="relative">
-            // Timeline node
-            <div className="absolute left-4 lg:left-1/2 w-4 h-4 bg-blue-500 rounded-full border-4 border-white lg:-translate-x-1/2"
-                 style={{ top: `${index * 200}px` }}></div>
-            
-            //</main> Timeline item 
-            <TimelineItem {...event} index={index} />
-          </div>
-        ))}
-      </div>
-    </div> 
-    */}
+      {/* Change the Top percentage value at the end*/}
+      <img src="images/robot-end.png" className="h-[98px] -mt-10 md:left-24 absolute min-[1170px]:left-48 lg:top-[48%] min-[744px]:h-[120px] min-[744px]:w-[120px] min-[1440px]:h-[210px] min-[1440px]:w-[218px]" />
+      <img src="images/robot-right.png" className="h-[113px] w-[141px] ml-auto mr-10 -mt-10 lg:-mt-52 lg:mr-20 min-[744px]:h-[171px] min-[744px]:w-[241px] min-[1440px]:h-[258px] min-[1440px]:w-[300px]" />
     </main>
   );
 }
