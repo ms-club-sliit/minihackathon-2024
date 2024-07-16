772d98fb035eb1856957597763bda6b4d3e96443
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2024-07-17 01:25:21 +0530

    update left image

--- app/_components/Home/PastWinner/index.jsx
+++ app/_components/Home/PastWinner/index.jsx
@@ -3,16 +3,11 @@ import PastWinnersData from "../../data/PastWinners.json";
 
 const PastWinner = () => {
   return (
-    <div className="relative mr-0">
-      <img 
-        src="images/design-left.png" 
-        alt="design-left" 
-        className="absolute flex flex-row justify-start py-28 bg-sponsors-bg bg-cover bg-center overflow-hidden "/>
-      
+    <>
+
       {PastWinnersData.data.map((winner) => (
         
-<div className="grid grid-cols-1 md:grid-cols-2 mb-5 md:mx-auto px-7 py-7 border-2 border-gray-200 rounded-3xl max-w-6xl mx-auto mx-6">
-
+        <div className="grid grid-cols-1 md:grid-cols-2 mb-5 md:mx-auto px-7 py-7 border-2 border-gray-200 rounded-3xl max-w-6xl mx-auto  mx-6 bg-white">
 
           {winner.id % 2 === 0 ? (
             <>
@@ -35,7 +30,7 @@ const PastWinner = () => {
               </div>
             </>
           ) : (
-            <>
+            < >
               <div className="flex justify-start">
                 <img
                   src={winner.imageUrl}
@@ -51,8 +46,9 @@ const PastWinner = () => {
             </>
           )}
         </div>
+        
       ))}
-    </div>
+    </>
   );
 };
 
--- components/Home/PastWinner/index.jsx
+++ components/Home/PastWinner/index.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import PastWinner from "../../../app/_components/Home/PastWinner";
 
-
 const PastWinnersList = () => {
-
   return (
-    <div className="lg:px-20 my-5">
-      <div>
-        <div className="flex justify-center p-6 rounded-lg my-10">
-          <h1 className="font-bold text-4xl md:text-5xl text-center">
-            Past Winners
-          </h1>
+    <div className="relative mr-0">
+      <img
+        src="images/design-left.png"
+        alt="design-left"
+        className="absolute opacity-[50%] h-[760px] md:h-[800px] md:w-auto left-0 top-1/2 transform -translate-y-1/2 bg-sponsors-bg bg-cover bg-center overflow-hidden "
+        />
+      <div className="lg:px-20 my-5">
+        <div>
+          <div className="flex justify-center p-6 rounded-lg my-10">
+            <h1 className="font-bold text-4xl md:text-5xl text-center">
+              Past Winners
+            </h1>
+          </div>
         </div>
+        <PastWinner />
       </div>
-      <PastWinner />
     </div>
   );
 };
