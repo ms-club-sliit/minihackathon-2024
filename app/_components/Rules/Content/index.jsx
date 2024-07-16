f5082ed754ec3e2d5a5db32a03731c6aebf478b8
Sahan Perera <sahanvimukthi00@gmail.com>
2024-07-17 01:41:46 +0530

    style: added code changes related to mobile view of the rules page

--- app/_components/Rules/Content/index.jsx
+++ app/_components/Rules/Content/index.jsx
@@ -7,16 +7,18 @@ export default function Content() {
         <div key={index} className="my-10">
           <h2
             key={index}
-            className="text-xl lg:text-3xl font-bold text-center lg:text-left"
+            className="text-xl lg:text-3xl font-bold text-center lg:text-left uppercase"
           >
             {section.title}
           </h2>
-          <p
-            key={index}
-            className="lg:pl-20 mt-5 text-[#334155] text-base lg:text-xl my-5 mx-10 lg:mx-0"
-          >
-            {section.content}
-          </p>
+          {section.content && (
+            <p
+              key={index}
+              className="lg:pl-20 mt-5 text-[#334155] text-base lg:text-xl my-5 mx-10 lg:mx-0"
+            >
+              {section.content}
+            </p>
+          )}
           {section.list && (
             <ul className="list-disc text-[#334155] text-base lg:text-xl pl-20 lg:pl-40 my-5 mr-10 lg:mr-0">
               {section.list.map((item, index) => (
@@ -43,11 +45,22 @@ export default function Content() {
           {section.rounds &&
             section.rounds.map((round, index) => (
               <div key={index} className="my-10">
-                <h3 className="text-xl lg:text-3xl font-bold my-10 text-center lg:text-left">
+                <h3 className="text-xl lg:text-2xl font-bold my-5 text-center lg:text-left lg:pl-20">
                   {round.title}
                 </h3>
                 <div className="text-[#334155] text-base lg:text-xl lg:pl-20 mx-10 lg:mx-0">
-                  <p>{round.content}</p>
+                  {round.content.map((content, index) => (
+                    <p key={index} className="mb-2">
+                      {content}
+                    </p>
+                  ))}
+                  {round.list && (
+                    <ul className="list-disc text-[#334155] text-base lg:text-xl pl-10 lg:pl-20 my-5">
+                      {round.list.map((item, i) => (
+                        <li key={i}>{item}</li>
+                      ))}
+                    </ul>
+                  )}
                   {round.advancement && (
                     <>
                       <h4 className="mt-10 font-bold">
@@ -61,13 +74,7 @@ export default function Content() {
                       )}
                     </>
                   )}
-                  {round.list && (
-                    <ul className="list-disc text-[#334155] text-base lg:text-xl pl-10 lg:pl-20 my-5">
-                      {round.list.map((item, i) => (
-                        <li key={i}>{item}</li>
-                      ))}
-                    </ul>
-                  )}
+                  {round.content2 && <p className="mb-2">{round.content2}</p>}
                 </div>
               </div>
             ))}
