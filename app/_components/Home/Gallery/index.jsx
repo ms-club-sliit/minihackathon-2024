9a9cf059bf4640e4e2ff37f025ad9f935d80943e
shafdo <shalinda@lesstaxi.com>
2024-07-21 03:26:54 +0530

    Fix: Resolved changes

--- app/_components/Home/Gallery/index.jsx
+++ app/_components/Home/Gallery/index.jsx
@@ -1,7 +1,7 @@
+import Image from 'next/image';
 import Marquee from 'react-fast-marquee';
 import galleryJson from '@/app/data/home/gallery.json';
 
-// TODO: Create a Gallery component.
 export default function Gallery() {
   return (
     <main>
@@ -11,55 +11,49 @@ export default function Gallery() {
 
       <div className="gallery space-y-4 py-16 container block mx-auto overflow-x-hidden">
         <div className="flex overflow-x-hidden">
-          <Marquee
-            gradient={false}
-            speed={40}
-            direction="left"
-            className="flex"
-          >
-            {galleryJson.row1.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt="slider-img"
-                className="w-[30rem] h-auto mr-4 rounded-3xl"
-              />
-            ))}
+          <Marquee gradient={false} speed={40} direction="left">
+            {galleryJson.row1 &&
+              galleryJson.row1.map((image, index) => (
+                <Image
+                  key={index}
+                  src={image}
+                  alt="slider-img"
+                  className="pr-4 rounded-3xl"
+                  width="480"
+                  height="320"
+                />
+              ))}
           </Marquee>
         </div>
         <div className="flex overflow-x-hidden">
-          <Marquee
-            gradient={false}
-            speed={40}
-            direction="right"
-            className="flex"
-          >
-            {galleryJson.row2.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt="slider-img"
-                className="w-[30rem] h-auto mr-4 rounded-3xl"
-              />
-            ))}
+          <Marquee gradient={false} speed={40} direction="right">
+            {galleryJson.row2 &&
+              galleryJson.row2.map((image, index) => (
+                <Image
+                  key={index}
+                  src={image}
+                  alt="slider-img"
+                  className="pr-4 rounded-3xl"
+                  width="480"
+                  height="320"
+                />
+              ))}
           </Marquee>
         </div>
 
         <div className="flex overflow-x-hidden">
-          <Marquee
-            gradient={false}
-            speed={40}
-            direction="left"
-            className="flex"
-          >
-            {galleryJson.row3.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt="slider-img"
-                className="w-[30rem] h-auto mr-4 rounded-3xl"
-              />
-            ))}
+          <Marquee gradient={false} speed={40} direction="left">
+            {galleryJson.row3 &&
+              galleryJson.row3.map((image, index) => (
+                <Image
+                  key={index}
+                  src={image}
+                  alt="slider-img"
+                  className="pr-4 rounded-3xl"
+                  width="480"
+                  height="320"
+                />
+              ))}
           </Marquee>
         </div>
       </div>
