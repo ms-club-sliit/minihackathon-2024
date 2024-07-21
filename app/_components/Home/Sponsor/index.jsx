b76ab9f0b2351cd06a28d95a25855050b77448ed
Randula Mawaththa <randula98@gmail.com>
2024-07-21 11:57:42 +0530

    Finalizing feats
    Signed-off-by: Randula Mawaththa <randula98@gmail.com>

--- app/_components/Home/Sponsor/index.jsx
+++ app/_components/Home/Sponsor/index.jsx
@@ -3,23 +3,23 @@ import sponsor from "@/data/home/sponsors.json";
 // Use transparent sponsor logos for best effect
 const Sponsors = () => {
   if (!sponsor) {
-    return null; 
+    return null;
   }
   return (
     <div className="text-center py-8">
       {Object.keys(sponsor).map((index) => (
         <div key={index}>
-          <h2 className="text-6xl font-bold">{index}</h2>
+          <h2 className="text-2xl font-bold">{index}</h2>
           <div className="flex flex-row justify-center py-8 bg-sponsors-bg bg-cover bg-center overflow-hidden">
             {sponsor[index] && sponsor[index].length > 0 && (
               sponsor[index].map((Sponsor) => (
                 <div key={index} className="items-center mx-4">
                   <Image
                     className="rounded-xl"
-                    src={Sponsor.src||'/images/home/sponsors/default.webp'}
-                    alt={Sponsor.alt||'Image Not Found'}
-                    width={Sponsor.width|| 300}
-                    height={Sponsor.height|| 300}
+                    src={Sponsor.src || '/images/home/sponsors/default.webp'}
+                    alt={Sponsor.alt || 'Image Not Found'}
+                    width={Sponsor.width || 300}
+                    height={Sponsor.height || 300}
                   />
                 </div>
               ))
--- components/Home/PastWinner/index.jsx
+++ components/Home/PastWinner/index.jsx
@@ -7,9 +7,9 @@ const PastWinnersList = () => {
       <img
         src="images/design-left.png"
         alt="design-left"
-        className="absolute opacity-[50%] h-[760px] md:h-[800px] md:w-auto left-0 top-1/2 transform -translate-y-1/2 bg-sponsors-bg bg-cover bg-center overflow-hidden "
-        />
-      <div className="lg:px-20 my-5">
+        className="absolute opacity-[50%] h-[760px] md:h-[800px] md:w-auto left-0 top-1/2 transform  bg-sponsors-bg bg-cover bg-center overflow-hidden "
+      />
+      <div className="lg:px-20 my-5 z-10">
         <div>
           <div className="flex justify-center p-6 rounded-lg my-10">
             <h1 className="font-bold text-4xl md:text-5xl text-center">
