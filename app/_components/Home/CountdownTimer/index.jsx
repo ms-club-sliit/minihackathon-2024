5426f77d0d2e82e68857f70b0d5746081a3ca92b
Sahan Perera <sahanvimukthi00@gmail.com>
2024-07-22 05:59:55 +0530

    style: added code changes related to ui style changes

--- app/_components/Home/CountdownTimer/index.jsx
+++ app/_components/Home/CountdownTimer/index.jsx
@@ -1,19 +1,24 @@
 // import Timer from './timer'
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 const Timer = dynamic(() => import("./timer"), {
-    ssr: false,
+  ssr: false,
 });
 export default function CountdownTimer() {
-    return (
-        <>
-            <div className="bg-sponsors-bg bg-cover bg-center overflow-hidden ">
-                <div className="row text-center">
-                    <h2 className="text-2xl md:text-4xl text-neutral-500">Awareness Session</h2>
-                    <h2 className="text-3xl md:text-5xl font-bold mt-2">Coming Soon!</h2>
-                </div>
-                <div className="row mt-3 w-full h-auto"><Timer /></div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="bg-sponsors-bg bg-cover bg-center overflow-hidden">
+        <div className="row text-center lg:pb-10">
+          <h2 className="text-lg lg:text-2xl text-neutral-500">
+            Awareness Session
+          </h2>
+          <h2 className="text-3xl lg:text-5xl font-semibold mt-2">
+            Coming Soon!
+          </h2>
+        </div>
+        <div className="row my-5 w-full h-auto">
+          <Timer />
+        </div>
+      </div>
+    </>
+  );
 }
-
